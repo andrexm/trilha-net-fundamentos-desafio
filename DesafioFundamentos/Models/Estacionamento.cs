5a938fd851eba28ebe275fc19975d303ba8d0b3b
André Nascimento <andre.neospace@gmail.com>
2025-08-01 23:43:03 -0300

    feat: add vehicle

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,9 +14,13 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
+            string placa = "";
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            while (placa == "")
+            {
+                placa = Console.ReadLine();
+            }
+            veiculos.Add(placa);
         }
 
         public void RemoverVeiculo()
